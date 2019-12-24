c9aabf6d272700e2f6fd93ebf9ff70538f19b83a
KSF <1291114093@qq.com>
2019-12-25 03:37:05 +0800

    sec

---
+++ Web/src/main/java/kasei/boot/web/config/security/CustomFilter.java
@@ -0,0 +1,22 @@
+package kasei.boot.web.config.security;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class CustomFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+}
--- Web/src/main/java/kasei/boot/web/config/security/CustomWebSecurityConfigurerAdapter.java
+++ Web/src/main/java/kasei/boot/web/config/security/CustomWebSecurityConfigurerAdapter.java
@@ -1,23 +1,35 @@
 package kasei.boot.web.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.autoconfigure.security.servlet.StaticResourceRequest;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.access.vote.RoleVoter;
 import org.springframework.security.authentication.AnonymousAuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -95,29 +107,140 @@ import java.util.List;
  * */
 @Configuration // 当有一个自定义的 WebSecurityConfigurerAdapter 类型的 bean 存在于 IOC 容器中时，会关闭 SecurityAutoConfiguration 的自动配置
 @EnableWebSecurity // spring mvc 整合 spring security 必要的注解
+@Order(SecurityProperties.BASIC_AUTH_ORDER - 10) // 为当前自定义的过滤器链设置顺序
 public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
 
+    /************************************ Authenticate Configuration *****************************************/
+    /** TODO 覆盖 SpringBoot 自动配置的全局 AuthenticationManager，一般情况下不用配置，使用自动配置的即可 */
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+    // @Bean
+    // public AuthenticationManager authenticationManager() {
+    //
+    //
+    //     /** TODO AuthenticationProvider
+    //      * 作用：作为 ProviderManager 的认证链的节点
+    //      * */
+    //     List<AuthenticationProvider> providers = new ArrayList<>();
+    //     providers.add(customAuthenticationProvider);
+    //     providers.add(new AnonymousAuthenticationProvider("foobar")); // 添加匿名认证方式的认证
+    //
+    //
+    //     /** TODO AuthenticationManager Introduce
+    //      * 作用： 用于对一个不完整的 Authenticate 进行认证，
+    //      *      如果认证成功，则返回一个完整的 Authenticate，
+    //      *      如果认证出错，则抛出 AuthenticationException 异常，
+    //      *      如果返回 null，则说明当前 AuthenticationManager 无法解析当前的 Authentication
+    //      * ProviderManager:
+    //      *      是 SpringSecurity 自带的 AuthenticationManager 接口的实现类
+    //      *      作用：逐个使用 AuthenticationProvider 对不完整的 Authenticate 进行认证
+    //      *      可以配置一个 父 ProviderManager，当链中所有 AuthenticationProvider 都返回 null，那么会调用 parent 中的链进行认证
+    //      * */
+    //     AuthenticationManager manager = new ProviderManager(providers, null);
+    //
+    //     return manager;
+    // }
 
-    // 自定义个 AuthenticateManager
+
+    /** TODO 自定义全局（父） AuthenticationManager，和上面的 authenticationManager() 方法效果是一样的
+     * 由于此处的 builder 是通过 @Autowired 注入的，
+     * 所以此处 builder 构建出来的 AuthenticationManager 是全局的，
+     * 或者说是其他 AuthenticationManager 的父 AuthenticationManager
+     * */
+    // @Autowired
+    // public void initialize(AuthenticationManagerBuilder builder, DataSource dataSource) {
+    //     builder.jdbcAuthentication().dataSource(dataSource).withUser("dave")
+    //         .password("secret").roles("USER");
+    // }
+
+    /** TODO 自定义局部（子） AuthenticationManager
+     * 由于此处的 builder 是覆写方法得到的，
+     * 所以此处的 builder 只能构建出局部（子） AuthenticationManager
+     * 全局 AuthenticationManager 可以使用 @Autowired 注入到其他 @Bean 中，
+     * 但是子 AuthenticationManager 不行，因为子 AuthenticationManager 不再 IOC 容器中
+     * */
+    @Autowired
+    private DataSource dataSource;
+    // @Override
+    // public void configure(AuthenticationManagerBuilder builder) {
+    //     builder.jdbcAuthentication().dataSource(dataSource).withUser("dave")
+    //         .password("secret").roles("USER");
+    // }
+
+
+    /************************************ Authorize Configuration *****************************************/
+    /** TODO 自定义 AccessDecisionManager */
+    // @Bean
+    // public AccessDecisionManager accessDecisionManager() {
+    //
+    //     /** TODO AccessDecisionVoter
+    //      * 作用：作为 AccessDecisionManager 鉴权链的节点，执行实际鉴权动作
+    //      * AccessDecisionVoter#vote 返回 1 表示通过鉴权，0 表示弃权， -1 表示鉴权失败
+    //      *
+    //      * AccessDecisionVoter#vote 方法参数：
+    //      *      Authentication authentication: 一个完全的 认证方式实例
+    //      *      S object: 受权限控制的对象，比如一个方法，一个类等
+    //      *      Collection<ConfigAttribute> attributes: object 相关的权限配置信息，经常使用 SpEL 表达式来计算
+    //      * */
+    //     List<AccessDecisionVoter<?>> decisionVoters = new ArrayList<>();
+    //     decisionVoters.add(new RoleVoter()); // 有层级角色投票器，适用于角色嵌套
+    //
+    //
+    //     /** TODO AccessDecisionManager Introduce
+    //      * 作用：鉴权
+    //      *
+    //      * AffirmativeBased: 一票通过
+    //      *      是 SpringSecurity 自带的 AccessDecisionManager 接口的实现类，内部维护一个链式的 AccessDecisionVoter ，用于执行实际的鉴权过程
+    //      *      作用：只要有任何一个 AccessDecisionVoter 通过鉴权，则表明鉴权成功
+    //      * UnanimousBased: 全票通过
+    //      *
+    //      * ConsensusBased: 多数通过
+    //      * */
+    //     AccessDecisionManager manager = new AffirmativeBased(decisionVoters); // 一票通过
+    //     return manager;
+    // }
+
+
+    /************************************ Filter Configuration *****************************************/
+    /** TODO 往 Servlet 容器中添加 Filter 的方法
+     * 1. 在 IOC 容器中配置一个实现 Filter 接口的 Bean，该 Bean 带有 @Order 注解，或者实现 Order 接口
+     * 2. 在 FilterRegistrationBean 中配置
+     *
+     * TODO Spring Security Filter
+     * Spring Security 只在 Servlet 容器中添加了一个过滤器，该过滤是 FilterChainProxy 的实例，并存放在 IOC 容器中
+     *
+     * FilterChainProxy 内部包含多个过滤器链，每一个链匹配一个 URL 路径，每一个链包含多个 DelegatingFilterProxy 过滤器
+     * Spring Boot 默认添加 6 条过滤器，1-5 条匹配静态资源路径；最后一条匹配所有 URL 路径，用于处理 身份认证/权限管理/会话等操作
+     * */
     @Bean
-    public AuthenticationManager authenticationManager() {
+    @Order(Ordered.LOWEST_PRECEDENCE -10)
+    public CustomFilter customFilter(){
+
+        return new CustomFilter();
+    }
+
+    /** TODO 自定义过滤器链：
+     * 添加一个 WebSecurityConfigurerAdapter 或 WebSecurityConfigurer 类型的 Bean 到 IOC 中
+     * 每一组匹配都有一个独立的 WebSecurityConfigurerAdapter Bean
+     * */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+            .authorizeRequests()
+            .antMatchers("/", "/home", "/h2-console/**")
+            .permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin()
+            .permitAll()
+            .and()
+            .logout()
+            .permitAll();
 
 
-        /** TODO */
-        List<AuthenticationProvider> providers = new ArrayList<>();
-        providers.add(new CustomAuthenticationProvider());
-        providers.add(new AnonymousAuthenticationProvider("foobar"));
-
-
-        /** TODO AuthenticationManager Introduce
-         * 作用： 用于对一个不完整的 Authenticate 进行认证，并返回一个完整的 Authenticate，如果认证出错则抛异常
-         * ProviderManager:
-         *      是 SpringSecurity 自带的 AuthenticationManager 接口的实现类
-         *      作用：逐个使用 AuthenticationProvider 对不完整的 Authenticate 进行认证
-         * */
-        AuthenticationManager manager = new ProviderManager(providers);
-
-        return manager;
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
 
@@ -135,23 +258,5 @@ public class CustomWebSecurityConfigurerAdapter extends WebSecurityConfigurerAda
     }
 
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
 
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/home", "/h2-console/**")
-                .permitAll()
-                .anyRequest().authenticated()
-            .and()
-                .formLogin()
-                .permitAll()
-            .and()
-                .logout()
-                .permitAll();
-
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-    }
 }
